610ed211dd1b63cf263b9e5caff8a9e9da1d877f
ayan4m1 <andrew@bulletlogic.com>
2022-05-04 00:05:40 -0400

    add titleTemplate

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 import { Fragment } from 'react';
 import { Container } from 'react-bootstrap';
+import Helmet from 'react-helmet';
 
 import Header from 'components/Header';
 
 export default function Layout({ children }) {
   return (
     <Fragment>
+      <Helmet titleTemplate="Coilz - %s" />
       <Header />
       <Container>{children}</Container>
     </Fragment>
