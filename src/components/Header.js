02e33d3cfea13d88b422fcbd76a58b9009d8ba96
ayan4m1 <andrew@bulletlogic.com>
2022-05-03 23:06:32 -0400

    add tooltips to nav items in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,32 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const NavItem = ({ to, icon, label }) => (
-  <Nav.Link as={Link} to={to}>
-    <FontAwesomeIcon icon={icon} size="2x" />
-    {Boolean(label) && ` ${label}`}
-  </Nav.Link>
-);
+const NavItem = ({ to, icon, label, tooltip }) =>
+  tooltip ? (
+    <OverlayTrigger
+      trigger="hover"
+      placement="bottom"
+      overlay={(props) => <Tooltip {...props}>{tooltip}</Tooltip>}
+    >
+      <Nav.Link as={Link} to={to}>
+        <FontAwesomeIcon icon={icon} size="2x" />
+        {Boolean(label) && ` ${label}`}
+      </Nav.Link>
+    </OverlayTrigger>
+  ) : (
+    <Nav.Link as={Link} to={to}>
+      <FontAwesomeIcon icon={icon} size="2x" />
+      {Boolean(label) && ` ${label}`}
+    </Nav.Link>
+  );
 
 NavItem.propTypes = {
   to: PropTypes.string.isRequired,
   icon: PropTypes.string.isRequired,
-  label: PropTypes.string
+  label: PropTypes.string,
+  tooltip: PropTypes.string
 };
 
 export default function Header() {
@@ -25,17 +38,48 @@ export default function Header() {
       <Navbar.Toggle />
       <Navbar.Collapse>
         <Nav>
-          <NavItem to="/coils" icon="calculator" label="Coils" />
-          <NavItem to="/materials" icon="thermometer-full" label="Materials" />
+          <NavItem
+            to="/coils"
+            icon="calculator"
+            label="Coils"
+            tooltip="Calculate coil resistance and heat flux."
+          />
+          <NavItem
+            to="/materials"
+            icon="thermometer-full"
+            label="Materials"
+            tooltip="Customize coil materials."
+          />
           <NavItem
             to="/nicotine"
             icon="exclamation-triangle"
             label="Nicotine"
+            tooltip="Calculate how long your nicotine supply will last."
           />
-          <NavItem to="/cost" icon="dollar-sign" label="Cost" />
-          <NavItem to="/base" icon="clock" label="Base" />
-          <NavItem to="/mod" icon="bomb" label="Mod" />
-          <NavItem to="/wiring" icon="network-wired" label="Wiring" />
+          <NavItem
+            to="/base"
+            icon="clock"
+            label="Base"
+            tooltip="Calculate how long your base supply will last."
+          />
+          <NavItem
+            to="/cost"
+            icon="dollar-sign"
+            label="Cost"
+            tooltip="Calculate the cost of your e-liquid from its components."
+          />
+          <NavItem
+            to="/mod"
+            icon="bomb"
+            label="Mod"
+            tooltip="Calculate amp limit for mechanical and regulated mods."
+          />
+          <NavItem
+            to="/wiring"
+            icon="network-wired"
+            label="Wiring"
+            tooltip="Calculate wire gauge from length and current."
+          />
         </Nav>
         <Nav className="ms-auto me-2">
           <Nav.Link
