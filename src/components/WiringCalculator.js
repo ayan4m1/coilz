7c6bb71f4cf5ee2ba12011d105e71853266da714
ayan4m1 <andrew@bulletlogic.com>
2022-06-20 22:54:54 -0400

    add validation and clarity to WiringCalculator

--- src/components/WiringCalculator.js
+++ src/components/WiringCalculator.js
@@ -13,6 +13,7 @@ import {
 
 import { wires } from 'utils';
 import ResultsCard from './ResultsCard';
+import { useCallback } from 'react';
 
 const copperDensity = 8.96;
 const copperHeatCapacity = 0.385;
@@ -20,31 +21,65 @@ const puffTime = 3.5;
 const standardTemperature = 20;
 
 export default function WiringCalculator() {
-  const { values, handleChange } = useFormik({
+  const { errors, values, handleChange } = useFormik({
     initialValues: {
       wireLength: 0.1,
       maxVoltageDrop: 0.25,
       maxTempRise: 50,
       maxCurrent: 30
-    }
+    },
+    validate: useCallback(
+      ({ wireLength, maxVoltageDrop, maxTempRise, maxCurrent }) => {
+        const result = {};
+
+        if (wireLength <= 0) {
+          result.wireLength = 'Wire length must be greater than zero.';
+        }
+
+        if (maxVoltageDrop <= 0) {
+          result.maxVoltageDrop = 'Max voltage drop must be greater than zero.';
+        }
+
+        if (maxTempRise <= 0) {
+          result.maxTempRise = 'Max temp rise must be greater than zero.';
+        } else if (maxTempRise >= 250) {
+          result.maxTempRise = 'Max temp rise must be less than 250.';
+        }
+
+        if (maxCurrent <= 0) {
+          result.maxCurrent = 'Max current must be greater than zero.';
+        } else if (maxCurrent > 200) {
+          result.maxCurrent = 'Max current must be less than 200.';
+        }
+
+        return result;
+      },
+      []
+    )
   });
 
   let wireGauge = NaN,
     voltageDrop = 0,
     tempRise = 0;
 
-  for (const { gauge, diameter, ampacity } of wires) {
-    voltageDrop = values.maxCurrent * (2 * values.wireLength * ampacity);
-    const wastePower = voltageDrop * values.maxCurrent;
-    const wasteEnergy = wastePower / puffTime;
-    const wireVolume = Math.PI * Math.pow(diameter / 2, 2) * values.wireLength;
-    const wireMass = copperDensity * wireVolume;
+  if (!Object.values(errors).length) {
+    for (const { gauge, diameter, ampacity } of wires) {
+      voltageDrop = values.maxCurrent * (2 * values.wireLength * ampacity);
+      const wastePower = voltageDrop * values.maxCurrent;
+      const wasteEnergy = wastePower / puffTime;
+      const wireVolume =
+        Math.PI * Math.pow(diameter / 2, 2) * values.wireLength;
+      const wireMass = copperDensity * wireVolume;
 
-    tempRise = wasteEnergy / (copperHeatCapacity * wireMass);
+      tempRise = wasteEnergy / (copperHeatCapacity * wireMass);
 
-    if (voltageDrop < values.maxVoltageDrop && tempRise < values.maxTempRise) {
-      wireGauge = gauge;
-      break;
+      if (
+        voltageDrop < values.maxVoltageDrop &&
+        tempRise < values.maxTempRise
+      ) {
+        wireGauge = gauge;
+        break;
+      }
     }
   }
 
@@ -64,6 +99,7 @@ export default function WiringCalculator() {
                 <Form.Label>Wire Length</Form.Label>
                 <InputGroup>
                   <Form.Control
+                    isInvalid={Boolean(errors.wireLength)}
                     min="0"
                     name="wireLength"
                     onChange={handleChange}
@@ -71,12 +107,16 @@ export default function WiringCalculator() {
                     value={values.wireLength}
                   />
                   <InputGroup.Text>m</InputGroup.Text>
+                  <Form.Control.Feedback type="invalid">
+                    {errors.wireLength}
+                  </Form.Control.Feedback>
                 </InputGroup>
               </Form.Group>
               <Form.Group>
                 <Form.Label>Max. Voltage Drop</Form.Label>
                 <InputGroup>
                   <Form.Control
+                    isInvalid={Boolean(errors.maxVoltageDrop)}
                     min="0"
                     name="maxVoltageDrop"
                     onChange={handleChange}
@@ -84,12 +124,16 @@ export default function WiringCalculator() {
                     value={values.maxVoltageDrop}
                   />
                   <InputGroup.Text>V</InputGroup.Text>
+                  <Form.Control.Feedback type="invalid">
+                    {errors.maxVoltageDrop}
+                  </Form.Control.Feedback>
                 </InputGroup>
               </Form.Group>
               <Form.Group>
                 <Form.Label>Max. Temp Rise</Form.Label>
                 <InputGroup>
                   <Form.Control
+                    isInvalid={Boolean(errors.maxTempRise)}
                     min="0"
                     name="maxTempRise"
                     onChange={handleChange}
@@ -97,12 +141,16 @@ export default function WiringCalculator() {
                     value={values.maxTempRise}
                   />
                   <InputGroup.Text>&deg;C</InputGroup.Text>
+                  <Form.Control.Feedback type="invalid">
+                    {errors.maxTempRise}
+                  </Form.Control.Feedback>
                 </InputGroup>
               </Form.Group>
               <Form.Group>
                 <Form.Label>Max. Current</Form.Label>
                 <InputGroup>
                   <Form.Control
+                    isInvalid={Boolean(errors.maxCurrent)}
                     min="0"
                     name="maxCurrent"
                     onChange={handleChange}
@@ -110,6 +158,9 @@ export default function WiringCalculator() {
                     value={values.maxCurrent}
                   />
                   <InputGroup.Text>A</InputGroup.Text>
+                  <Form.Control.Feedback type="invalid">
+                    {errors.maxCurrent}
+                  </Form.Control.Feedback>
                 </InputGroup>
               </Form.Group>
             </Form>
@@ -125,9 +176,11 @@ export default function WiringCalculator() {
               <p>
                 You should use <strong>{wireGauge}</strong> AWG wire, which will
                 cause a drop of <strong>{voltageDrop.toFixed(2)}</strong> volts
-                and heat to roughly{' '}
+                and heat from <strong>{standardTemperature}</strong> &deg;C to
+                roughly{' '}
                 <strong>{(standardTemperature + tempRise).toFixed(2)}</strong>{' '}
-                &deg;C under a {values.maxCurrent} amp load.
+                &deg;C under a <strong>{values.maxCurrent}</strong> amp load for{' '}
+                <strong>{puffTime}</strong> seconds.
               </p>
             </ResultsCard>
           )}
