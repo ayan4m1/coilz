04d3a067b5b55fe6182bfb7896a84a1be03cbeb8
ayan4m1 <andrew@bulletlogic.com>
2022-10-25 18:16:58 -0400

    set container bg correctly for all pages

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,13 +4,16 @@ import { Container } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
 
 import Header from 'components/Header';
+import useDarkMode from 'hooks/useDarkMode';
 
 export default function Layout({ children }) {
+  const { value: darkMode } = useDarkMode();
+
   return (
     <Fragment>
       <Helmet titleTemplate="Coilz - %s" />
       <Header />
-      <Container>{children}</Container>
+      <Container bg={darkMode ? 'light' : 'dark'}>{children}</Container>
     </Fragment>
   );
 }
