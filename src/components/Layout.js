52134b5fbfd8ca800d68f5e12ddf218e23ce6300
ayan4m1 <andrew@bulletlogic.com>
2025-02-02 11:31:03 -0500

    upgrade deps and switch to new react router approach

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,20 +1,18 @@
-import PropTypes from 'prop-types';
 import { Fragment } from 'react';
-import { Container } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
+import { Outlet } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 import Header from 'components/Header';
 
-export default function Layout({ children }) {
+export default function Layout() {
   return (
     <Fragment>
       <Helmet titleTemplate="Coilz - %s" />
       <Header />
-      <Container>{children}</Container>
+      <Container>
+        <Outlet />
+      </Container>
     </Fragment>
   );
 }
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired
-};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { lazy, Suspense } from 'react';
+import { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 
 import './index.scss';
 
@@ -8,42 +8,46 @@ import Layout from 'components/Layout';
 import SuspenseFallback from 'components/SuspenseFallback.js';
 import ThemeProvider from 'components/ThemeProvider';
 
-const Home = lazy(() => import('pages/Home'));
-const MaterialEditor = lazy(() => import('pages/MaterialEditor'));
-const Settings = lazy(() => import('pages/Settings'));
-const MixCalculator = lazy(() => import('pages/MixCalculator'));
-const BaseCalculator = lazy(() => import('pages/BaseCalculator'));
-const CoilCalculator = lazy(() => import('pages/CoilCalculator'));
-const ModCalculator = lazy(() => import('pages/ModCalculator'));
-const NicCalculator = lazy(() => import('pages/NicCalculator'));
-const CostCalculator = lazy(() => import('pages/CostCalculator'));
-const WiringCalculator = lazy(() => import('pages/WiringCalculator'));
-const SpoolCalculator = lazy(() => import('pages/SpoolCalculator'));
-// const PowerCalculator = lazy(() => import('pages/PowerCalculator'));
+const createRouteForPage = (pathOrIndex, pageName) => {
+  const result = {};
 
+  if (typeof pathOrIndex === 'boolean') {
+    result.index = pathOrIndex;
+  } else if (typeof pathOrIndex === 'string') {
+    result.path = pathOrIndex;
+  }
+
+  result.lazy = async () => ({
+    Component: (await import(`pages/${pageName}`)).default
+  });
+
+  return result;
+};
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      createRouteForPage(true, 'Home'),
+      createRouteForPage('materials', 'MaterialEditor'),
+      createRouteForPage('settings', 'Settings'),
+      createRouteForPage('mix', 'MixCalculator'),
+      createRouteForPage('coils', 'CoilCalculator'),
+      createRouteForPage('nicotine', 'NicCalculator'),
+      createRouteForPage('mod', 'ModCalculator'),
+      createRouteForPage('base', 'BaseCalculator'),
+      createRouteForPage('cost', 'CostCalculator'),
+      createRouteForPage('wiring', 'WiringCalculator'),
+      createRouteForPage('spool', 'SpoolCalculator')
+    ]
+  }
+]);
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <Router>
-    <ThemeProvider>
-      <Layout>
-        <Suspense fallback={<SuspenseFallback />}>
-          <Routes>
-            <Route element={<Home />} index />
-            <Route element={<MaterialEditor />} path="/materials" />
-            <Route element={<Settings />} path="/settings" />
-            <Route element={<MixCalculator />} path="/mix" />
-            <Route element={<CoilCalculator />} path="/coils" />
-            <Route element={<NicCalculator />} path="/nicotine" />
-            <Route element={<ModCalculator />} path="/mod" />
-            <Route element={<BaseCalculator />} path="/base" />
-            <Route element={<CostCalculator />} path="/cost" />
-            <Route element={<WiringCalculator />} path="/wiring" />
-            <Route element={<SpoolCalculator />} path="/spool" />
-            {/* <Route element={<PowerCalculator />} path="/power" /> */}
-          </Routes>
-        </Suspense>
-      </Layout>
-    </ThemeProvider>
-  </Router>
+  <ThemeProvider>
+    <Suspense fallback={<SuspenseFallback />}>
+      <RouterProvider router={router} />
+    </Suspense>
+  </ThemeProvider>
 );
